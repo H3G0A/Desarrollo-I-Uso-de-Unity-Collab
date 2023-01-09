a4be8a9662017a1948bdb68718fc26204a382b11
Gonzolo27 <60710427+Gonzolo27@users.noreply.github.com>
2023-01-09 17:07:12 +0100

    feature(Des-I): Enemy model and animations

--- Assets/Scripts/PoolSpawners/BulletSpawner.cs
+++ Assets/Scripts/PoolSpawners/BulletSpawner.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BulletSpawner : Spawner
 {
     [SerializeField] GameObject bullet;
+    [SerializeField] Transform player;
 
     private void Start()
     {
@@ -11,12 +12,15 @@ public class BulletSpawner : Spawner
 
     public void SpawnBullet(Transform spawnTransform)
     {
+        Vector3 direction = player.position - spawnTransform.position;
+
         GameObject go = Spawn(bullet, spawnTransform);
         go.GetComponent<BulletController>().SetBulletSpawner(this);
         Rigidbody rb = go.GetComponent<Rigidbody>();
 
         rb.velocity = Vector3.zero;
-        rb.AddForce(spawnTransform.forward * 32f, ForceMode.Impulse);
+        //rb.AddForce(spawnTransform.forward * 32f, ForceMode.Impulse);
+        rb.AddForce(direction * 1.5f, ForceMode.Impulse);
     }
 
     public void DeSpawn(GameObject go)
